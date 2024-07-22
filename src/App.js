e1e344ecd395cc0fbd3003d8406bd6eb4fdb4024
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-07-22 14:54:33 +0330

    [package]- Change Routes Structure

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
 import React from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useRoutes } from "react-router-dom";
+import routes from "./routes";
 // * Components
 import SideBar from "./components/SideBar/SideBar";
 import Header from "./components/Header/Header";
-import Products from './components/Products/Products';
-import Comments from './components/Comments/Comments';
-import Users from './components/Users/Users';
-import Orders from './components/Orders/Orders';
-import Offs from './components/Offs/Offs';
-
 
 function App() {
+  const router = useRoutes(routes);
   return (
     <>
       <SideBar />
       <div className="main">
         <Header />
-        <Routes>
-          <Route path="/products" element={<Products />} />
-          <Route path="/comments" element={<Comments />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/offs" element={<Offs />} />
-        </Routes>
+        {router}
       </div>
     </>
   );
