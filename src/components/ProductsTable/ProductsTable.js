db13665f18b3efa95fd16ef0bdebe2c170f7a7f9
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-07-24 09:21:41 +0330

    [CSS]- Add CSS Styles For ProductsTable Section

--- src/components/ProductsTable/ProductsTable.js
+++ src/components/ProductsTable/ProductsTable.js
@@ -2,18 +2,19 @@ import React from "react";
 import "./ProductsTable.css";
 function ProductsTable() {
   return (
-    <div>
+    <div className="product-table-box">
       <table className="product-table">
         <tr className="product-table__heading-tr">
           <th className="product-table__heading-th">عکس</th>
           <th className="product-table__heading-th">اسم</th>
           <th className="product-table__heading-th">قیمت</th>
           <th className="product-table__heading-th">موجودی</th>
+          <th className="product-table__heading-th"></th>
         </tr>
 
-        <tr>
+        <tr className="products-table-tr">
             <td>
-                <img src="./images/admin.png" alt="product" className="product-table-img" />
+                <img src="./images/iphone15.jpg" alt="product" className="product-table-img" />
             </td>
             <td>
                 محصول
@@ -24,7 +25,27 @@ function ProductsTable() {
             <td>
                 82
             </td>
+            <td className="table-btns">
+                <button className="product-table-btn">جزئیات</button>
+                <button className="product-table-btn">حذف</button>
+                <button className="product-table-btn">ویرایش</button>
+            </td>
+        </tr>
+        
+        <tr className="products-table-tr">
             <td>
+                <img src="./images/iphone15.jpg" alt="product" className="product-table-img" />
+            </td>
+            <td>
+                محصول
+            </td>
+            <td>
+                92000 تومان
+            </td>
+            <td>
+                82
+            </td>
+            <td className="table-btns">
                 <button className="product-table-btn">جزئیات</button>
                 <button className="product-table-btn">حذف</button>
                 <button className="product-table-btn">ویرایش</button>
