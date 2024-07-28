e4f1a44662c0ccdd71a319510a1ccaab8a87b9b4
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-07-28 20:57:52 +0330

    update

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App";
 import "./custom.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter> 
     <App />
   </BrowserRouter>
 );
