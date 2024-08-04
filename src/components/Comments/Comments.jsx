fd2ab9fb8014487839e59360152cb295f1476b5e
mojtaba-jsx <mojtaba.jsx@gmail.com>
2024-08-04 10:25:03 +0330

    [Logic]- Add Logic For Delete And Edit Comments

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -4,25 +4,43 @@ import "./Comments.css";
 import ErrorBox from "../ErrorBox/ErrorBox";
 import DeleteModal from "../DeleteModal/DeleteModal";
 import DetailsModal from "../DetailsModal/DetailsModal";
+import EditModal from "../EditModal/EditModal";
 export default function Comments() {
   const [allComments, setAllComments] = useState([]);
   const [isShowDetailsModal, setIsShowDetailsModal] = useState(false);
   const [isShowDeleteModal, setIsShowDeleteModal] = useState(false);
+  const [isShowEditModal, setIsShowEditModal] = useState(false);
   const [mainCommentBody, setMainCommentBody] = useState("");
+  const [commentID, setCommentID] = useState(null);
 
   useEffect(() => {
+    gettAllComments();
+  }, []);
+
+  function gettAllComments() {
     fetch("http://localhost:8000/api/comments")
       .then((res) => res.json())
       .then((comments) => setAllComments(comments));
-  }, []);
+  }
 
   const closeDetailsModal = () => setIsShowDetailsModal(false);
   const closeDeleteModal = () => setIsShowDeleteModal(false);
+  const closeEditModal = () => setIsShowEditModal(false);
 
   const deleteComment = () => {
-    console.log('کامنت با موفقیت ریمو شد');
-    setIsShowDeleteModal(false)
-  }
+    fetch(`http://localhost:8000/api/comments/${commentID}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((result) => console.log(result));
+    setIsShowDeleteModal(false);
+    gettAllComments();
+  };
+
+  const updateComment = () => {
+    console.log("comment updated");
+    setIsShowEditModal(false);
+  };
 
   return (
     <div className="cms-main">
@@ -56,8 +74,22 @@ export default function Comments() {
                 <td>{comment.date}</td>
                 <td>{comment.hour}</td>
                 <td>
-                  <button onClick={() => setIsShowDeleteModal(true)}>حذف</button>
-                  <button>ویرایش</button>
+                  <button
+                    onClick={() => {
+                      setIsShowDeleteModal(true);
+                      setCommentID(comment.id);
+                    }}
+                  >
+                    حذف
+                  </button>
+                  <button
+                    onClick={() => {
+                      setIsShowEditModal(true);
+                      setMainCommentBody(comment.body);
+                    }}
+                  >
+                    ویرایش
+                  </button>
                   <button>پاسخ</button>
                   <button>تایید</button>
                 </td>
@@ -72,20 +104,25 @@ export default function Comments() {
       {isShowDetailsModal && (
         <DetailsModal onHide={closeDetailsModal}>
           <p className="text-modal">{mainCommentBody}</p>
-          <button className="text-modal-close-btn" onClick={closeDetailsModal}>بستن</button>
+          <button className="text-modal-close-btn" onClick={closeDetailsModal}>
+            بستن
+          </button>
         </DetailsModal>
       )}
 
-      {
-        isShowDeleteModal && (
-          <DeleteModal
-          cancel={closeDeleteModal}
-          submit={deleteComment}
-          />
-        )
-      }
+      {isShowDeleteModal && (
+        <DeleteModal cancel={closeDeleteModal} submit={deleteComment} />
+      )}
+
+      {isShowEditModal && (
+        <EditModal cancel={closeEditModal} submit={updateComment}>
+          <textarea
+            onChange={(event) => setMainCommentBody(event.target.value)}
+            className="edit-comment-text"
+            value={mainCommentBody}
+          ></textarea>
+        </EditModal>
+      )}
     </div>
   );
 }
-
-
